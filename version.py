24aaf5e36da4b32bbade77f877838a3bdba6ddf8
leah558 <johnleahfeb@gmail.com>
2026-03-01 10:19:55 -0500

    Add files via upload

--- version.py
+++ version.py
@@ -1,11 +1,3 @@
+from ._version import __version__
 
-"""
-Module to expose more detailed version info for the installed `numpy`
-"""
-version = "2.4.2"
-__version__ = version
-full_version = version
-
-git_revision = "c81c49f77451340651a751e76bca607d85e4fd55"
-release = 'dev' not in version and '+' not in version
-short_version = version.split("+")[0]
+VERSION: str = __version__
